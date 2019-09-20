40c57f5143160ff97e49f6ad3548f543490f63ca
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-20 22:16:08 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,11 +2,11 @@ module Memorable
   
   module ClassMethods
     def count
-      all.count
+      self.all.count
     end
     
     def clear
-      all.clear
+      self.all.clear
     end
   end
   
